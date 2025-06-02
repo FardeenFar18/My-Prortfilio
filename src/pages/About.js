7c2e50f8ccbaba220fcf5a369b445153150ea193
FardeenFar18 <118423654+FardeenFar18@users.noreply.github.com>
2025-06-02 18:17:51 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <h2 className="about-title">About Me</h2>
         <p className="about-description">
           I am a passionate <span className="highlight">MERN Stack Developer</span> with 
-          <span className="highlight"> 1.4 years</span> of experience in building 
+          <span className="highlight"> 1.6 years</span> of experience in building 
           scalable and high-performance web applications. I have expertise in 
           <span className="highlight"> React.js, Node.js, Express.js, MongoDB, and PostgreSQL</span>.  
           Additionally, I have experience in <span className="highlight">manual testing</span>, ensuring 
